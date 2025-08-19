c8229cf68c6931ef5999c21dc7f744fa295c6b85
JozefGranec <65823367+JozefGranec@users.noreply.github.com>
2025-08-19 21:49:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,11 +7,12 @@ st.set_page_config(page_title="Food Planner", page_icon="🍽️", layout="wide"
 st.title("🍽️ Food Planner")
 
 # Create tables if first run
-create_db_and_tables()
+# create_db_and_tables()
 
 tab1, tab2, tab3 = st.tabs(["Cook Book", "Food Plan", "Shopping List"])
 with tab1: cookbook.render()
 with tab2: food_plan.render()
 with tab3: shopping_list.render()
+
 
 
