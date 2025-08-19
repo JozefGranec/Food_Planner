2329e6e2adf346be5bddd6cab954e365850af647
JozefGranec <65823367+JozefGranec@users.noreply.github.com>
2025-08-19 21:47:29 +0200

    Update db.py

--- food/db.py
+++ food/db.py
@@ -17,7 +17,7 @@ def _connect() -> sqlite3.Connection:
 def _table_info(conn: sqlite3.Connection, table: str):
     return conn.execute(f"PRAGMA table_info({table});").fetchall()
 
-def init_db() -> None:
+def create_db_and_tables() -> None:
     with _connect() as conn:
         conn.execute(
             """
